99519b1dd125487a6e343627d5ff946c46f42d14
dennis githinji <wakahiad@gmail.com>
2023-05-21 13:43:24 +0300

    project setup

---
+++ src/main/java/com/densoft/msscbearservice/MsscBearServiceApplication.java
@@ -0,0 +1,13 @@
+package com.densoft.msscbearservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBearServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscBearServiceApplication.class, args);
+	}
+
+}
