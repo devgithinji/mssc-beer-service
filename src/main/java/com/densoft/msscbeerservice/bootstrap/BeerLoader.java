93a6165e471a39b13826a98f87c5b28e08357ffd
dennis githinji <wakahiad@gmail.com>
2023-05-21 19:56:52 +0300

    add bootstrap data

---
+++ src/main/java/com/densoft/msscbeerservice/bootstrap/BeerLoader.java
@@ -0,0 +1,48 @@
+package com.densoft.msscbeerservice.bootstrap;
+
+import com.densoft.msscbeerservice.domain.Beer;
+import com.densoft.msscbeerservice.respositories.BeerRepository;
+import com.densoft.msscbeerservice.web.model.BeerStyleEnum;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class BeerLoader implements CommandLineRunner {
+
+    private final BeerRepository beerRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        loadBeerObjects();
+    }
+
+    private void loadBeerObjects() {
+        if (beerRepository.count() == 0) {
+            beerRepository.save(Beer.builder()
+                    .beerName("Mango Bobs")
+                    .beerStyle(BeerStyleEnum.IPA.toString())
+                    .quantityToBrew(200)
+                    .minOnHand(12)
+                    .upc(337010000001L)
+                    .price(new BigDecimal("12.95"))
+                    .build());
+
+            beerRepository.save(Beer.builder()
+                    .beerName("Galaxy Cat")
+                    .beerStyle(BeerStyleEnum.PALE_ALE.toString())
+                    .quantityToBrew(200)
+                    .minOnHand(12)
+                    .upc(337010000002L)
+                    .price(new BigDecimal("11.95"))
+                    .build());
+        }
+
+        log.info("Loaded beers: " + beerRepository.count());
+    }
+}
