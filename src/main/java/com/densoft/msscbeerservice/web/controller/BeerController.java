f15ff704d45c3b4db50036d6ad33b262202f6197
dennis githinji <wakahiad@gmail.com>
2023-05-22 14:22:45 +0300

    gloabl error handling

--- src/main/java/com/densoft/msscbeerservice/web/controller/BeerController.java
+++ src/main/java/com/densoft/msscbeerservice/web/controller/BeerController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.UUID;
 
 @RestController
@@ -17,13 +18,13 @@ public class BeerController {
     }
 
     @PostMapping
-    public ResponseEntity saveBeer(@RequestBody BeerDto beerDto) {
+    public ResponseEntity saveBeer(@Valid @RequestBody BeerDto beerDto) {
         // TODO impl
         return new ResponseEntity(HttpStatus.CREATED);
     }
 
     @PutMapping("/{beerId}")
-    public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto) {
+    public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId,@Valid @RequestBody BeerDto beerDto) {
         // TODO impl
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
